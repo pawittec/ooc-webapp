04651d4dadd6f4c617339c332acdcaad7c692a04
Palm <palm@Palms-MacBook-Pro.local>
2017-02-18 21:48:27 +0700

    final

--- src/main/java/io/muic/ooc/webapp/Webapp.java
+++ src/main/java/io/muic/ooc/webapp/Webapp.java
@@ -3,9 +3,12 @@ package io.muic.ooc.webapp;
 import io.muic.ooc.webapp.service.SecurityService;
 import java.io.File;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.descriptor.web.ErrorPage;
 
 /**
  * Created by gigadot on 02-Feb-17.
@@ -22,17 +25,26 @@ public class Webapp {
         ServletRouter servletRouter = new ServletRouter();
         servletRouter.setSecurityService(securityService);
 
+        ErrorPage errorPage = new ErrorPage();
+        errorPage.setErrorCode(HttpServletResponse.SC_NOT_FOUND);
+        errorPage.setLocation("/");
         Context ctx;
+
         try {
             ctx = tomcat.addWebapp("/", new File(docBase).getAbsolutePath());
             servletRouter.init(ctx);
+            ctx.addErrorPage(errorPage);
             tomcat.start();
             tomcat.getServer().await();
         } catch (ServletException | LifecycleException ex) {
             ex.printStackTrace();
         }
 
-        
+
+
+
+
+
     }
 
 
